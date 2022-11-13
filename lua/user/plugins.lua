4f40a77f48c45a3b66ab566687a2387ec7b555ad
Hizbullah Abdul Aziz Jabbar <archbung@tutanota.com>
2022-11-14 05:53:12 +0700

    Unpin plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -38,55 +38,38 @@ packer.init {
 
 return packer.startup(function(use)
 
-  use {
-    "wbthomason/packer.nvim", 
-    commit = "6afb67460283f0e990d35d229fd38fdc04063e0a",
-  }
+  use "wbthomason/packer.nvim"
 
-  use {
-    "nathom/filetype.nvim",
-    commit = "b522628a45a17d58fc0073ffd64f9dc9530a8027",
-  }
+  use "nathom/filetype.nvim"
 
-  use {
-    "lewis6991/impatient.nvim",
-    commit = "b842e16ecc1a700f62adb9802f8355b99b52a5a6",
-  }
+  use "lewis6991/impatient.nvim"
 
-  use {
-    "windwp/nvim-autopairs",
-    commit = "14cc2a4fc6243152ba085cc2059834113496c60a",
-  }
+  use "windwp/nvim-autopairs"
 
 
   -- Treesitter
   use {
     "nvim-treesitter/nvim-treesitter",
-    commit = "8ec59aee8097c64fcf27d1dbd77ea181c50846c5",
     run = ":TSUpdate",
   }
 
   use { 
     "nvim-treesitter/nvim-treesitter-refactor", 
-    commit = "75f5895cc662d61eb919da8050b7a0124400d589",
     requires = "nvim-treesitter/nvim-treesitter",
   }
 
   use { 
     "nvim-treesitter/nvim-treesitter-textobjects",
-    commit = "e63c2ff8e38fad77299dd74e14c7c9360e1b3181",
     requires = "nvim-treesitter/nvim-treesitter",
   }
 
   use {
     "nvim-treesitter/nvim-treesitter-context",
-    commit = "c46a8a0a60412a8fe43aa6bd3a01845c46de6bf2",
     requires = "nvim-treesitter/nvim-treesitter",
   }
 
   use {
     "p00f/nvim-ts-rainbow",
-    commig = "1ec3f880585c644ddd50a51502c59f4e36f03e62",
     requires = "nvim-treesitter/nvim-treesitter",
   }
 
@@ -103,7 +86,6 @@ return packer.startup(function(use)
 
   use {
     "nvim-telescope/telescope-frecency.nvim",
-    commit = "d51c7631dcc0f598692676f554c4e79d7596d541",
     requires = {
       "nvim-telescope/telescope.nvim",
       "kkharji/sqlite.lua",
@@ -112,7 +94,6 @@ return packer.startup(function(use)
 
   use {
     "nvim-telescope/telescope-fzy-native.nvim",
-    commit = "282f069504515eec762ab6d6c89903377252bf5b",
     requires = {
       "nvim-telescope/telescope.nvim",
     },
@@ -121,38 +102,27 @@ return packer.startup(function(use)
 
 
   -- which-key
-  use {
-    "folke/which-key.nvim",
-    commit = "6885b669523ff4238de99a7c653d47b081b5506d",
-  }  
+  use "folke/which-key.nvim"
 
 
   -- Git
   use {
     "TimUntersberger/neogit",
-    commit = "74c9e29b61780345d3ad9d7a4a4437607caead4a",
     requires = {
       "nvim-lua/plenary.nvim",
     },
   }
 
-  use {
-    "lewis6991/gitsigns.nvim",
-    commit = "5df4ac570e796f3ccf1d9c0445c131f6225bde8c",
-  }
+  use "lewis6991/gitsigns.nvim"
 
 
   -- Eye candies
   use {
     "catppuccin/nvim",
-    commit = "0447ffc0050c43a0d7a23ce3640d22a5ef781605",
     as = "catppuccin",
   }
 
-  use {
-    "lukas-reineke/indent-blankline.nvim",
-    commit = "db7cbcb40cc00fc5d6074d7569fb37197705e7f6",
-  }
+  use "lukas-reineke/indent-blankline.nvim"
 
   use {
     "kyazdani42/nvim-tree.lua",
@@ -160,15 +130,6 @@ return packer.startup(function(use)
       "kyazdani42/nvim-web-devicons",
     },
     tag = "nightly",
-  }
-
-
-  -- Lang
-  -- neorg
-  use {
-    "nvim-neorg/neorg",
-    tag = "*", 
-    requires = "nvim-lua/plenary.nvim",
   }
 
   if PACKER_BOOTSTRAP then
