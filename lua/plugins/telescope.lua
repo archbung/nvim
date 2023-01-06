525855b7f6dfaa001e839f743b791bc081400644
Hizbullah Abdul Aziz Jabbar <archbung@tutanota.com>
2023-01-07 04:53:13 +0700

    Add telescope-zoxide

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,17 +4,25 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "kyazdani42/nvim-web-devicons",
-    { 
+    {
       "nvim-telescope/telescope-frecency.nvim",
       dependencies = "kkharji/sqlite.lua",
     },
-    { 
+    {
       "nvim-telescope/telescope-fzy-native.nvim",
       build = "make",
+    },
+    {
+      "jvgrootveld/telescope-zoxide",
+      dependencies = {
+        "nvim-lua/popup.nvim",
+        "nvim-lua/plenary.nvim",
+      },
     },
   },
   config = function()
     local actions = require("telescope.actions")
+    local z_utils = require("telescope._extensions.zoxide.utils")
 
     require("telescope").setup({
       path_display = { "smart", },
@@ -76,9 +84,27 @@ return {
           override_generic_sorter = false,
           override_file_sorter = true,
         },
+        zoxide = {
+          mappings = {
+            default = {
+              after_action = function(selection)
+                print("Update to (" .. selection.z_score .. ") " .. selection.path)
+              end
+            },
+            ["<C-s>"] = {
+              before_action = function() print("before C-s") end,
+              action = function(selection)
+                vim.cmd("edit " .. selection.path)
+              end
+            },
+            -- Opens the selected entry in a new split
+            ["<C-q>"] = { action = z_utils.create_basic_command("split") },
+          },
+        },
       },
     })
 
     require("telescope").load_extension("frecency")
+    require("telescope").load_extension("zoxide")
   end,
 }
