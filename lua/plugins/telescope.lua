1fb433fc220bcb39a4645fd37e6393b2959e098a
Hizbullah Abdul Aziz Jabbar <archbung@tutanota.com>
2023-01-10 12:29:41 +0700

    Update

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
   cmd = { "Telescope", },
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "kyazdani42/nvim-web-devicons",
+    "nvim-tree/nvim-web-devicons",
     {
       "nvim-telescope/telescope-frecency.nvim",
       dependencies = "kkharji/sqlite.lua",
